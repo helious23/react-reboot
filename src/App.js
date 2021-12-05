f66b5e479444faa8c1a1df838cfb91ebc15533f1
Max16 <max16@naver.com>
2021-12-05 09:36:15 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [coinValue, setCoinValue] = useState({});
+  const [amount, setAmount] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onAmountChange = (event) => {
+    setAmount(event.target.value);
   };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+  const onCoinChange = (event) => {
+    setCoinValue(event.target.value);
+    console.log(coinValue);
   };
+
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do.."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <div>
+            <select onChange={onCoinChange}>
+              <option value="0">Choose The Coin!</option>
+              {coins.map((coin) => (
+                <option key={coin.id} value={coin.quotes.USD.price}>
+                  {coin.name} ({coin.symbol}) $ {coin.quotes.USD.price} USD
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="amount">
+              How much $ you want to convert to coin?
+            </label>
+            <input
+              id="amount"
+              value={amount}
+              type="number"
+              placeholder="Write amout $"
+              onChange={onAmountChange}
+            />
+          </div>
+          <div>
+            You can buy {parseInt(amount) / coinValue} {}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
